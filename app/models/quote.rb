2272794f42c8b35fb8bfdc866fccbf11334b6488
esi <espersell@gmail.com>
2019-07-28 19:28:42 -0500

    Automatic price adding in quoting form

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,9 +1,8 @@
 class Quote < ApplicationRecord
-  validates :markup_percentage, presence: true, if: :above_minimum
+  validates :markup_percentage,presence: true
+  validates_numericality_of :markup_percentage, :greater_than => 15, :less_than => 100
+  validates :manufacturer_id, presence: false
 
-  def above_minimum
-    #markup_percentage >= QuoteConfigsController.getMin
-  end
 
   def getSuggestedLP(num)
     #statement = "Select suggested_list_price from list_prices where model_id = num"
@@ -12,15 +11,14 @@ class Quote < ApplicationRecord
 
 
   belongs_to :customer
-  / belongs_to :list_price /
   belongs_to :manufacturer
+  / belongs_to :discount /
   belongs_to :series
   belongs_to :model
   belongs_to :tire_replacement_front
   belongs_to :tire_replacement_rear
   belongs_to :attachment
   belongs_to :implement
-  / belongs_to :quote_config /
   has_many :specific_equipments
 
 
